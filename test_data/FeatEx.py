ffdfabec72cba39c9b4e12a3856e3d8b436b62b7
lordwarlock <zheng@ZhihaoZhengdeMacBook-Pro.local>
2015-02-01 17:56:46 -0500

    update
    process both raw and gold

--- test_data/FeatEx.py
+++ test_data/FeatEx.py
@@ -2,9 +2,11 @@ import re
 import nltk
 from feature_functions import get_feature_functions_list
 class FeatureExtraction(object):
-    def __init__(self):
+    def __init__(self,raw_flag=0):
         self.data = []
         self.feat = []
+        self.raw_flag = raw_flag
+        self.feat_length = 4 - raw_flag
     def read_origin_train(self,filename):
         with open(filename,'r') as indata:
             for line in indata:
@@ -12,20 +14,48 @@ class FeatureExtraction(object):
 
     def separate_store_train(self,line):
         if (line != '\n') :
-            match = re.match('(.*)\t(.*)\t(.*)\t(.*)\n',line)
-            if (match != None):
-                self.data.append(match.groups())
+            if self.raw_flag:
+                match = re.match('(.*)\t(.*)\t(.*)\n',line)
+                if (match != None):
+                    self.data.append(match.groups()+tuple(['']))
+            else:
+                match = re.match('(.*)\t(.*)\t(.*)\t(.*)\n',line)
+                if (match != None):
+                    self.data.append(match.groups())
 
     def feature_extraction(self):
         if ( self.data == [] ): return
         else:
             feat_func_list = get_feature_functions_list()
+            self.feat_length += len(feat_func_list)
             for line in self.data:
-                self.feat.append(  list(line[:-1])\
-                                 + [func(line) for func in feat_func_list]
-                                 + [line[-1]])
+                if self.raw_flag:
+                    self.feat.append(  list(line[:-1])\
+                                     + [func(line) for func in feat_func_list])
+                else:
+                    self.feat.append(  list(line[:-1])\
+                                     + [func(line) for func in feat_func_list]
+                                     + [line[-1]])            
+
+    def output_feat(self,filename):
+        with open(filename,'w') as output_file:
+            for line in self.feat:
+                i = 0
+                for feat in line:
+                    i += 1
+                    output_file.write(str(feat))
+                    if (i != self.feat_length):
+                        output_file.write('\t')
+                    else:
+                        output_file.write('\n')
+     
 if __name__ == '__main__':
     fe = FeatureExtraction()
     fe.read_origin_train('dev.gold')
     fe.feature_extraction()
-    print fe.feat
+    fe.output_feat('zzh_test.gold')
+    fe1 = FeatureExtraction(1)
+    fe1.read_origin_train('dev.raw')
+    fe1.feature_extraction()
+    fe1.output_feat('zzh_test.raw')
+
--- test_data/feature_functions.py
+++ test_data/feature_functions.py
@@ -1,9 +1,29 @@
-def first_cap(data):
-    no, word, pos_tag, ne_tag = data
-    if (word[0] >= 'A' and word[0] <= 'Z'):
+def is_cap(char):
+    if (char >= 'A' and char <= 'Z'):
         return 1
     else:
         return 0
 
+def feat_first_cap(data):
+    no, word, pos_tag, ne_tag = data
+    return is_cap(word[0])
+
+def feat_first_word(data):
+    no, word, pos_tag, ne_tag = data
+    if (no == '0'): return 1
+    else: return 0
+
+def feat_cap_period(data):
+    no, word, pos_tag, ne_tag = data
+    cap_flag = 0
+    per_flag = 0
+    for char in word:
+        if is_cap(char) : 
+            cap_flag = 1
+        if (char == '.') :
+            per_flag = 1
+    return cap_flag*per_flag
+
 def get_feature_functions_list():
-    return [first_cap]
+    
+    return [feat_first_cap,feat_first_word,feat_cap_period]
